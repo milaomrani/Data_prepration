4d476ab9d0dd1538aaf71ef3938ef2428fdf46d3
Milad Omrani <50385765+milaomrani@users.noreply.github.com>
2022-12-25 20:58:01 -0500

    Update data_prepration.py

--- data_prepration.py
+++ data_prepration.py
@@ -54,3 +54,24 @@ X = X.reshape(X.shape+(1,)) #dimensions to be the same for X and Y
 print(X.shape)
 print(Y.shape)
 
+
+# Read data and convert from RGB to LAB using openCV
+img_data_LAB=[]
+img_data_org = []
+path1 = '****' # copy and paste where your images are located ex: "lol_dataset/lol_dataset/our485/low/"
+files1=os.listdir(path1)
+for i in tqdm(files1):
+    img=cv2.imread(path1+'/'+i,1)   #Change 0 to 1 for color images
+    img=cv2.resize(img,(SIZE, SIZE))
+    img=cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    avg_a = np.average(img[:, :, 1])
+    avg_b = np.average(img[:, :, 2])
+    img[:, :, 1] = img[:, :, 1] - ((avg_a - 128) * (img[:, :, 0] / 255.0) * 1.2)
+    img[:, :, 2] = img[:, :, 2] - ((avg_b - 128) * (img[:, :, 0] / 255.0) * 1.2)
+    img = cv2.cvtColor(img, cv2.COLOR_LAB2BGR)
+    
+    img_data_LAB.append(img_to_array(img))
+    
+
+img_array_LAB = np.reshape(img_data_LAB, (len(img_data_LAB), SIZE, SIZE, 3))
+img_array_LAB = img_array_LAB.astype('float32') / 255.
